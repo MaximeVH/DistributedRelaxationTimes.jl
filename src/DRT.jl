e378bfec599dc4ac147ad00adc0fc59c55dd0f25
MaximeVH <maxime.vanhaeverbeke@gmail.com>
2022-09-30 11:39:30 +0200

    Name change

--- src/DRT.jl
+++ src/DRT.jl
@@ -2,7 +2,7 @@ module DRT
 
     export compute_DRT
     
-    using Optim, QuadGK, Roots, Statistics, ToeplitzMatrices, LinearAlgebra
+    using Optim, QuadGK, Roots, Statistics, ToeplitzMatrices, LinearAlgebra, Plots
 
     include("construct_matrices.jl")
     include("quad_format.jl")
