db99ad95f7f85eeac961e3e07e9702a8d0696a55
MaximeVH <maxime.vanhaeverbeke@gmail.com>
2023-02-08 16:17:41 +0100

    drt plotting

--- src/visualisations.jl
+++ src/visualisations.jl
@@ -1,9 +1,27 @@
-function plot_DRT(taus,gammas,lab="DRT",title_ = "Distribution of relaxation times")
-    fig = Plots.plot(taus,gammas,xaxis=:log,label = lab,ylabel = "γ",xlabel = "τ", title = title_)
+function plot_DRT(taus,gammas,lab="DRT",title_ = "Distribution of relaxation times";color = :auto)
+    fig = Plots.plot(taus,gammas,xaxis=:log,label = lab,ylabel = "γ (Ω)",xlabel = "τ (s)", title = title_,linecolor=color)
     return fig
 end
 
-function plot_DRT!(taus,gammas,lab="DRT")
-    fig = Plots.plot!(taus,gammas,xaxis=:log,label = lab,ylabel = "γ",xlabel = "τ")
+function plot_DRT!(taus,gammas,lab="DRT";color = :auto)
+    fig = Plots.plot!(taus,gammas,xaxis=:log,label = lab,ylabel = "γ (Ω)",xlabel = "τ (s)",linecolor=color)
+    return fig
+end
+
+# methods with dashed lines at the peaks
+
+function plot_DRT(peak_times,peak_amps,taus,gammas,lab="DRT",title_ = "Distribution of relaxation times";color = :auto)
+    fig = Plots.plot(taus,gammas,xaxis=:log,label = lab,ylabel = "γ (Ω)",xlabel = "τ (s)", title = title_,linecolor=color)
+    for (time,amp) in zip(peak_times,peak_amps)
+        Plots.plot!([time,time],[0,amp],linestyle=:dash,linewidth = 0.5, linecolor=:black, label=nothing)
+    end
+    return fig
+end
+
+function plot_DRT!(peak_times,peak_amps,taus,gammas,lab="DRT";color = :auto)
+    fig = Plots.plot!(taus,gammas,xaxis=:log,label = lab,ylabel = "γ (Ω)",xlabel = "τ (s)",linecolor=color)
+    for (time,amp) in zip(peak_times,peak_amps)
+        Plots.plot!([time,time],[0,amp],linestyle=:dash,linewidth = 0.5, linecolor=:black, label=nothing)
+    end
     return fig
 end
